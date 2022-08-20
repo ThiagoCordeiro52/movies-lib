f5b0d8debea56fe37337a9ffa03bed06583ffa77
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-08-20 19:06:39 -0300

    :sparkles: feat: movie details page created

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -3,6 +3,11 @@ type Movie = {
   id: number;
   poster_path: string;
   vote_average: number;
+  tagline: string;
+  budget: number;
+  revenue: number;
+  runtime: number;
+  overview: string;
 };
 
 export default Movie;
